24dc4ea5c2150e734684cdf7201e610e2f0c48af
Md. Jahid Hasan <mdjahidhasanf92@gmail.com>
2021-09-15 16:43:15 +0600

    Lab task1 [Protfolio design]

---
+++ Lab Task-1[Portfolio Design]/webapp/Controllers/PortfolioController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webapp.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Biio()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
